fa8e0a4df76c55a3b398c63d992481c2b90c19c5
jorgeeliecerpalacios <jorge_palacios_1987@hotmail.com>
2022-09-30 15:55:45 -0500

    install mysql and config db mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',       #solo_cambia_mysql
+        'OPTIONS': {
+            'host': 'localhost',
+            'database': 'mysite',         #nombre_base_de_datos
+            'user': 'root',
+            'password': '',
+        },
     }
 }
 
