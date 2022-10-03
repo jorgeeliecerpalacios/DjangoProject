8b18b98b74617e970cc47810affd1176f99f564f
jorgeeliecerpalacios <jorge_palacios_1987@hotmail.com>
2022-10-02 20:23:59 -0500

    created templates

--- myapp/models.py
+++ myapp/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Project(models.Model):
     
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
     
-    tittle = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title + ' / ' + self.project.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Index Page')
+    title = 'Django course!!!'
+    return render(request,'index.html', {'title': title})
+
+def about(request):
+    username = 'Jorge'
+    return render(request,'about.html', {'username': username})
 
 def hello(request, username):
     # print(username)
     return HttpResponse('<h1>hello %s </h1>' % username)
 
-def about(request):
-    return HttpResponse('About')
 
 def projects(request):
-    projects = list(Project.objects.all())
-    return JsonResponse(projects, safe=False)
+    # projects = list(Project.objects.all())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {'projects': projects})
 
-def tasks(request, id):
-    tasks = get_object_or_404(Task, id=id)
-    return HttpResponse('tasks,: %s' % tasks.tittle)
+def tasks(request):
+    # tasks = Task.objects.get(title=title)
+    # tasks = get_object_or_404(Task, id=id)
+    return render(request,'task.html')
